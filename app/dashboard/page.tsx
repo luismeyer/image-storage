9befe1d89ce2bae448c5beaf9f92b77aa451ef0d
Luis Meyer <luis_meyer@outlook.de>
2023-10-04 22:43:12 +0200

    feat: add download button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function Dashboard() {
         <LogoutButton />
       </div>
 
-      <section className="w-1/2 bg-gray-200 p-4 overflow-y-auto">
+      <section className="w-1/2 bg-gray-100 p-4 overflow-y-auto">
         <Posts />
       </section>
     </main>
--- components/logic/posts.tsx
+++ components/logic/posts.tsx
@@ -1,8 +1,10 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import Link from "next/link";
 
 import { list } from "@/app/api/list";
+import { buttonVariants } from "@/components/ui/button";
 
 dayjs.extend(relativeTime);
 
@@ -11,26 +13,52 @@ export async function Posts() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full gap-4 overflow-scroll">
-      {posts?.map((image, index) => (
+      {posts?.map(({ title, image, author, created_at }, index) => (
         <div
-          key={image.title + index}
-          className="border border-gray-300 rounded overflow-hidden relative h-fit"
+          key={title + index}
+          className="group border border-gray-300 rounded overflow-hidden relative h-fit shadow-lg"
         >
           <Image
             className="w-full object-contain"
             alt="Uploaded image 1"
             height="300"
             width="200"
-            src={image.image}
+            src={image}
           />
 
           <div className="bg-black text-white w-full p-2">
             <span className="text-sm font-thin">
-              {image.author} - {dayjs(image.created_at).fromNow()}
+              {author} - {dayjs(created_at).fromNow()}
             </span>
 
-            <h3 className="text-xl font-bold">{image.title}</h3>
+            <h3 className="text-xl font-bold">{title}</h3>
           </div>
+
+          <Link
+            href={image}
+            download
+            className={buttonVariants({
+              variant: "outline",
+              size: "icon",
+              className:
+                "absolute top-2 right-2 shadow-md group-hover:visible invisible",
+            })}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </Link>
         </div>
       ))}
     </div>
