44ba93136d8a1fa6f76f72dad7d296ae27b04779
Luis Meyer <luis_meyer@outlook.de>
2023-10-05 11:42:19 +0200

    feat: restrict access to vercel org members

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,15 @@ if (!process.env.GITHUB_SECRET) {
   throw new Error("Missing env var: GITHUB_SECRET");
 }
 
+if (!process.env.VERCEL_ORG_ID) {
+  throw new Error("Missing env var: VERCEL_ORG_ID");
+}
+
+type Org = {
+  id: number;
+  login: string;
+};
+
 const handler = NextAuth({
   providers: [
     GithubProvider({
@@ -16,6 +25,25 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    signIn: async ({ profile }) => {
+      if (
+        profile &&
+        "organizations_url" in profile &&
+        typeof profile.organizations_url === "string"
+      ) {
+        const response = await fetch(profile.organizations_url);
+        const orgs: Org[] = await response.json();
+
+        return orgs.some(
+          ({ id, login }) =>
+            login === "vercel" && String(id) === process.env.VERCEL_ORG_ID
+        );
+      }
+
+      return false;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
